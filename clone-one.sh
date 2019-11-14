9a3bedeafc1d811787716d19760080059670091c
yemiancheng <ymc.github@gmail.com>
2019-11-14 09:58:21 +0800

    refator(core):updating clone one

--- clone-one.sh
+++ clone-one.sh
@@ -6,7 +6,10 @@
 # 列出运行中的主机
 # 关闭被克隆的主机
 # 克隆出某新的主机
+# 免密登录新的主机
 # 修改主机名字地址
+# 修改域名解析地址
+
 
 VMs_PATH="D:\\VirtualBox\\Administrator\\VMs"
 mkdir -p $VMs_PATH
@@ -18,114 +21,138 @@ cd $VM_PATH
 PATH_SPLIT_SYMBOL="\\"
 VM_BASE_PATH=${VMs_PATH}${PATH_SPLIT_SYMBOL}${VM_PATH}
 echo $VM_BASE_PATH
+VM_NAME=$VM_PATH
 
-NEW_VM_PATH=centos-7.6-clone
+NEW_VM_PATH=k8s-node-6
 NEW_VM_BASE_FOLEDR=${VMs_PATH}${PATH_SPLIT_SYMBOL}${NEW_VM_PATH}
 NEW_VM_NAME=$NEW_VM_PATH
-# 列出
-VBoxManage list runningvms | sed "s#{.*}##g" | grep $VM_NAME
-# 关闭
-if [ $? -eq 0 ];then
-    VBoxManage controlvm $VM_NAME poweroff
-    sleep 5
-fi
-# 克隆
-VBoxManage clonevm $VM_NAME --name $NEW_VM_NAME --register --basefolder $VMs_PATH
-#time VBoxManage clonevm $VM_NAME --name $NEW_VM_NAME --register --basefolder $VMs_PATH
-sleep 10
-# 启动
-VBoxManage startvm $NEW_VM_NAME --type headless
-# 登录
-#ssh root@192.168.2.2
 PRIVITE_KEY_FILE_NAME=google-clound-ssr
 PRIVITE_KEY_FILE_PATH=~/.ssh/
 OLD_VM_SSH_SERVER_IP=192.168.2.2
 OLD_VM_SSH_SERVER_USER=root
-# ssh -i ${PRIVITE_KEY_FILE_PATH}${PRIVITE_KEY_FILE_NAME} $OLD_VM_SSH_SERVER_IP@$OLD_VM_SSH_SERVER_USER
-ssh -t -t -i ${PRIVITE_KEY_FILE_PATH}${PRIVITE_KEY_FILE_NAME} $OLD_VM_SSH_SERVER_IP@$OLD_VM_SSH_SERVER_USER << run-some-task-on-vm-node
+NEW_VM_SSH_SERVER_IP=192.168.2.6
+NEW_VM_SSH_SERVER_USER=root
+NEW_VM_HOST_NAME=$NEW_VM_NAME
+
+#某个网络
+ONE_NETWORK=192.168.2.0/24
+#网卡名字
+NEW_VM_NET_CARD_NAME=eth0
+#电脑地址
+NEW_VM_IPADDR=$NEW_VM_SSH_SERVER_IP #192.168.2.2
+#网络掩码
+NEW_VM_NETMASK=255.255.255.0
+#网关地址
+NEW_VM_GATEWAY=192.168.2.1
+
+
+
+# 关闭
+VBoxManage list runningvms | sed "s#{.*}##g" | grep $VM_NAME
+if [ $? -eq 0 ];then
+    VBoxManage controlvm $VM_NAME poweroff
+    sleep 10
+fi
+# 克隆
+VBoxManage list vms | sed "s#{.*}##g" | grep $NEW_VM_NAME
+if [ $? -eq 0 ]
+then
+    echo "need to clone a vm" > /dev/null 2>&1
+else 
+    VBoxManage clonevm $VM_NAME --name $NEW_VM_NAME --register --basefolder $VMs_PATH
+    sleep 40
+fi
+# 启动
+VBoxManage list runningvms | sed "s#{.*}##g" | grep $NEW_VM_NAME
+if [ $? -eq 0 ]
+then
+  echo "has been started before" > /dev/null 2>&1
+else
+  VBoxManage startvm $NEW_VM_NAME --type headless
+fi
+sleep 60
+
+# 登录
+#ssh root@192.168.2.2
+# ssh -i ${PRIVITE_KEY_FILE_PATH}${PRIVITE_KEY_FILE_NAME} $OLD_VM_SSH_SERVER_USER@$OLD_VM_SSH_SERVER_IP
+# ssh -tt -i ${PRIVITE_KEY_FILE_PATH}${PRIVITE_KEY_FILE_NAME} $OLD_VM_SSH_SERVER_USER@$OLD_VM_SSH_SERVER_IP
+ssh -i ${PRIVITE_KEY_FILE_PATH}${PRIVITE_KEY_FILE_NAME} $OLD_VM_SSH_SERVER_USER@$OLD_VM_SSH_SERVER_IP << run-some-task-on-vm-node
+
 
 ####
 #functions
 ####
 function update_static_ip(){
-#网卡名字
-NET_CARD_NAME=eth0
-#电脑地址
-VM_IPADDR=$1 #192.168.2.2
-#网络掩码
-VM_NETMASK=255.255.255.0
-#网关地址
-VM_GATEWAY=192.168.2.1
-#某个网络
-ONE_NETWORK=192.168.2.0/24
-#命令操作
-ACTION="SET" #SET|REVOCER
-#cat /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME} | grep --extended-regexp "IPADDR.*(.*.)\..*"
-
 #设置
-sed -i 's/BOOTPROTO=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME}
-sed -i 's/ONBOOT=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME}
-sed -i 's/IPADDR=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME}
-sed -i 's/NETMASK=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME}
-sed -i 's/GATEWAY=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME}
-sed -i '/^\s*$/d' /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME} # 删除空格
-cat >> /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME} <<centos-set-static-ip-address
-IPADDR=${VM_IPADDR}
-NETMASK=${VM_NETMASK}
-GATEWAY=${VM_GATEWAY}
+if [ -e /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME} ]
+then
+echo "update ip in net card :${NEW_VM_NET_CARD_NAME}"
+sed -i 's/IPADDR=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME}
+sed -i 's/NETMASK=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME}
+sed -i 's/GATEWAY=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME}
+sed -i 's/BOOTPROTO=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME}
+sed -i 's/ONBOOT=.*//g' /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME}
+sed -i '/^\s*$/d' /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME} # 删除空格
+fi
+cat >> /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME} <<centos-set-static-ip-address
+IPADDR=${NEW_VM_IPADDR}
+NETMASK=${NEW_VM_NETMASK}
+GATEWAY=${NEW_VM_GATEWAY}
 BOOTPROTO=static
 ONBOOT=yes
 centos-set-static-ip-address
 
 #service network restart
 # 查看
-cat /etc/sysconfig/network-scripts/ifcfg-${NET_CARD_NAME} | grep --extended-regexp "IPADDR.*(.*.)\..*"
+cat /etc/sysconfig/network-scripts/ifcfg-${NEW_VM_NET_CARD_NAME} | grep --extended-regexp "IPADDR.*(.*.)\..*"
 }
 
 function set_host_name(){
-HOST_NAME=$1 #k8s-master
 # 设置
 #2 for centos7
-hostnamectl set-hostname $HOST_NAME
+hostnamectl set-hostname $NEW_VM_HOST_NAME
 #2 for centos6
-#hostname $HOST_NAME
+#hostname $NEW_VM_HOST_NAME
+
 # 查看
 cat /etc/hostname
 
 
 # 设置
-cat /etc/hosts | grep "127.0.0.1" | grep $HOST_NAME > /dev/null 2>&1
-if [ $? -eq 0 ];then
-  echo "yes" > /dev/null 2>&1
-  else
+#cat /etc/hosts | grep "127.0.0.1" | grep "${NEW_VM_HOST_NAME}" > /dev/null 2>&1
+
   #echo  "no"
   # 在匹配的行前添加#(注释)
   #sed  '/127.0.0.1 */ s/^/#/g' /etc/hosts
   # 在匹配的行后添加（追加）
-  #sed -i "/127.0.0.1 */ s/$/ $HOST_NAME/g" /etc/hosts
+  #sed -i "/127.0.0.1 */ s/$/ $NEW_VM_HOST_NAME/g" /etc/hosts
   # 删除后添加（覆盖）
   #sed "s/127.0.0.1.*//g" /etc/hosts | sed '/^\s*$/d'
   sed -i "s/127.0.0.1.*//g" /etc/hosts
   sed -i '/^\s*$/d' /etc/hosts
-  echo "127.0.0.1 $HOST_NAME" >> /etc/hosts
-fi
+  echo "127.0.0.1 $NEW_VM_HOST_NAME" >> /etc/hosts
 
-cat /etc/hosts | grep "::1" | grep $HOST_NAME > /dev/null 2>&1
-if [ $? -eq 0 ];then
-  echo "yes" > /dev/null 2>&1
-  else
+#cat /etc/hosts | grep "::1" | grep $NEW_VM_HOST_NAME > /dev/null 2>&1
   #echo  "no"
   # 在匹配的行前添加#
   #sed  '/::1 */ s/^/#/g' /etc/hosts
   # 在匹配的行后添加#
-  #sed -i "/::1 */ s/$/ $HOST_NAME/g" /etc/hosts
+  #sed -i "/::1 */ s/$/ $NEW_VM_HOST_NAME/g" /etc/hosts
   sed -i "s/::1.*//g" /etc/hosts
   sed -i '/^\s*$/d' /etc/hosts
-  echo "::1 $HOST_NAME" >> /etc/hosts
-fi
+  echo "::1 $NEW_VM_HOST_NAME" >> /etc/hosts
 
 # 查看
 cat /etc/hosts
+}
+
+function set_dns_resovle_in_china(){
+  cat > /etc/resolv.conf  << set-host_dns_resovle
+nameserver 223.5.5.5
+nameserver 223.6.6.6
+set-host_dns_resovle
+
+  cat /etc/resolv.conf
 }
 
 ####
@@ -133,11 +160,37 @@ cat /etc/hosts
 ####
 # 操作
 #2 修改ip
-update_static_ip 192.168.2.3
+update_static_ip
 #2 修改hostname
-set_host_name k8s-node-3
+set_host_name
+#2 修改dns域名服务器
+set_dns_resovle_in_china
 #2 ...
-# 重启
+# 退出
 sleep 10
-reboot
+exit
 run-some-task-on-vm-node
+# 关机
+VBoxManage list runningvms | sed "s#{.*}##g" | grep $NEW_VM_NAME
+if [ $? -eq 0 ]
+then
+  sleep 3
+  VBoxManage controlvm $NEW_VM_NAME poweroff
+fi
+sleep 40
+
+# 重启
+VBoxManage list runningvms | sed "s#{.*}##g" | grep $NEW_VM_NAME
+if [ $? -eq 0 ]
+then
+  echo "has been started before" > /dev/null 2>&1
+else
+  VBoxManage startvm $NEW_VM_NAME --type headless
+fi
+sleep 60
+# 免密登录
+ssh -t -i ${PRIVITE_KEY_FILE_PATH}${PRIVITE_KEY_FILE_NAME} $NEW_VM_SSH_SERVER_USER@$NEW_VM_SSH_SERVER_IP
+
+
+#### usage
+# bash ./clone-one.sh
